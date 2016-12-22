7e990eb5dbed982d9b995b29623c3abf1f84f118
wblock <wblock@wonkity.com>
2016-12-22 11:43:56 -0700

    Update for upcoming 9.10.2-U1 version.

--- userguide/conf.py
+++ userguide/conf.py
@@ -27,9 +27,9 @@ copyright = u'2011-2016, iXsystems'
 # built documents.
 #
 # The short X.Y version.
-version = '9.10.2'
+version = '9.10.2-U1'
 # The full version, including alpha/beta/rc tags.
-release = '9.10.2'
+release = '9.10.2-U1'
 
 # exclude_patterns is a list of patterns relative to the source directory
 # that match files and directories to ignore when looking for source files.
