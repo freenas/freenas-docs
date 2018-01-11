3a5b1294ab299ce4922eceb0781ee98f75b0b840
dlavigne <dru@freebsd.org>
2018-01-11 11:50:32 -0500

    Prep for U1.

--- userguide/conf.py
+++ userguide/conf.py
@@ -29,13 +29,13 @@ copyright = '2011-2017, iXsystems'
 #
 
 # VERSION is the LONG, FULL version number with all patch levels, like "11.0-U1"
-version = '11.1-RELEASE'
+version = '11.1-U1'
 # RELEASE is the short major release number ONLY, like "11.0"
 release = '11.1'
 
 if tags.has('truenas'):
     # VERSION is the LONG, FULL, version number
-    version = '11.1-RELEASE'
+    version = '11.1-U1'
     # RELEASE is the short major release number ONLY
     release = '11.1'
 
