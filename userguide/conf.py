cefa8ba036533bf894941b044c2b8aa651049486
dlavigne <dru@freebsd.org>
2018-05-24 09:00:38 -0400

    Prep for TN-11.1-U5 and FN-11.2-BETA1.

--- userguide/conf.py
+++ userguide/conf.py
@@ -30,13 +30,13 @@ copyright = '2011-2018, iXsystems'
 #
 
 # VERSION is the LONG, FULL version number with all patch levels, like "11.0-U1"
-version = '11.1-U4'
+version = '11.2-BETA1'
 # RELEASE is the short major release number ONLY, like "11.0"
-release = '11.1'
+release = '11.2'
 
 if tags.has('truenas'):
     # VERSION is the LONG, FULL, version number
-    version = '11.1-U4'
+    version = '11.1-U5'
     # RELEASE is the short major release number ONLY
     release = '11.1'
 
