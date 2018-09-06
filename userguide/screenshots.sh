f40b7a8fb4b29a7e6cc0e23f1e142ed130356db8
wblock <wblock@wonkity.com>
2018-09-06 09:36:44 -0400

    Add utility script to report on and copy screenshots

---
+++ userguide/screenshots.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+IMAGEDIR="images"
+TMPDIR="/tmp"
+IMAGELIST="${TMPDIR}/userguide-imagelist.txt"
+IMAGESCRIPT="${TMPDIR}/imagecopy.sh"
+MANUALIMAGES="${TMPDIR}/manual-imagelist.txt"
+
+IMAGES=$( ls ${IMAGEDIR}/*.png )
+
+echo ${IMAGES} > ${IMAGELIST}
+echo "list of all User Guide images: ${IMAGELIST}"
+
+echo "SRCDIR=\"changeme\"" > ${IMAGESCRIPT}
+echo "DESTDIR=\"changemetoo\"" >> ${IMAGESCRIPT}
+for image in ${IMAGES}; do
+  name=$( basename $image )
+  echo "scp \${SRCDIR}/$name \${DESTDIR}/$name" >> ${IMAGESCRIPT}
+done
+echo "image copy script: ${IMAGESCRIPT}"
+
+echo "Oddly-sized, probably manually-created images" > ${MANUALIMAGES}
+echo > ${MANUALIMAGES}
+for image in ${IMAGES}; do
+  wxh=$( identify $image | cut -w -f3 )
+  height=$( echo $wxh | cut -dx -f2 )
+  if [ $height -lt 1049 ];then
+    echo "manual image: $image $wxh" > ${MANUALIMAGES}
+  fi
+done
+echo "manual images list: ${MANUALIMAGES}"
