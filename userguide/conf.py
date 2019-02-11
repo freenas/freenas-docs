5d7b8432e8b3324518620ce79734e09cfba90a7f
Warren Block <wblock@wonkity.com>
2019-02-11 14:53:45 -0500

    Line these up for readability

--- userguide/conf.py
+++ userguide/conf.py
@@ -177,8 +177,8 @@ rst_prolog = u'''
 
 # roles for text replacement
 rst_prolog += u'''
-.. |web-ui| replace:: web interface
-.. |dockerhost| replace:: Docker Host
+.. |web-ui|         replace:: web interface
+.. |dockerhost|     replace:: Docker Host
 .. |copyright-year| replace:: 2019
 '''
 
