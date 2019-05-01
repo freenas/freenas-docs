fda8db7042be607a39d74724cbfb203b73586e08
dlavigne <dru@freebsd.org>
2019-05-01 12:23:51 -0400

    Prep for U8.

--- userguide/conf.py
+++ userguide/conf.py
@@ -30,7 +30,7 @@ copyright = '2011-2019, iXsystems'
 #
 
 # VERSION is the LONG, FULL version number with all patch levels, like "11.0-U1"
-version = '11.1-U7'
+version = '11.1-U8'
 # RELEASE is the short major release number ONLY, like "11.0"
 release = '11.1'
 
