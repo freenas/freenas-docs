bb7592a5a44e406bff7a23be6400fb5b5184f7eb
Warren Block <wblock@wonkity.com>
2019-06-05 12:59:54 -0400

    Change "storage controller" to "TrueNAS controller"

--- userguide/conf.py
+++ userguide/conf.py
@@ -78,16 +78,16 @@ rst_prolog = u'''
 .. |dockerhost|           replace:: Docker VM
 .. |help-pin|             replace::  (Pin)
 .. |help-unpin|           replace::  (Unpin)
-.. |Ctrlr-term|           replace:: Storage Controller
-.. |Ctrlrs-term|          replace:: Storage Controllers
-.. |Ctrlr-term-1|         replace:: Storage Controller 1
-.. |Ctrlr-term-2|         replace:: Storage Controller 2
-.. |Ctrlr-term-1-2|       replace:: Storage Controller 1/2
-.. |ctrlr-term|           replace:: storage controller
-.. |ctrlrs-term|          replace:: storage controllers
-.. |ctrlr-term-1|         replace:: storage controller 1
-.. |ctrlr-term-2|         replace:: storage controller 2
-.. |ctrlr-term-1-2|       replace:: storage controller 1/2
+.. |Ctrlr-term|           replace:: TrueNAS Controller
+.. |Ctrlrs-term|          replace:: TrueNAS Controllers
+.. |Ctrlr-term-1|         replace:: TrueNAS Controller 1
+.. |Ctrlr-term-2|         replace:: TrueNAS Controller 2
+.. |Ctrlr-term-1-2|       replace:: TrueNAS Controller 1/2
+.. |ctrlr-term|           replace:: TrueNAS controller
+.. |ctrlrs-term|          replace:: TrueNAS controllers
+.. |ctrlr-term-1|         replace:: TrueNAS controller 1
+.. |ctrlr-term-2|         replace:: TrueNAS controller 2
+.. |ctrlr-term-1-2|       replace:: TrueNAS controller 1/2
 .. |os-device|            replace:: operating system device
 .. |os-devices|           replace:: operating system devices
 .. |OS-Device|            replace:: Operating System Device
