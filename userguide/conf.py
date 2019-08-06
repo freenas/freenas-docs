b530f945a18ef5480084d6fd87b578f387c6d196
wblock <wblock@wonkity.com>
2019-08-06 09:07:16 -0400

    Update for TN-11.2-U5.1

--- userguide/conf.py
+++ userguide/conf.py
@@ -54,7 +54,7 @@ if tags.has('truenas'):
     tags.add('truenas')
     master_doc = 'truenas'
 
-    version = '11.2-U5'
+    version = '11.2-U5.1'
     release = '11.2'
     product = f'{brand} {version} User Guide'
 
