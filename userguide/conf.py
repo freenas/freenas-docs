1fc89b62cb50ab93f7e392da1d7c10246df0b239
Aaron <32651598+a-stjohn@users.noreply.github.com>
2019-12-23 09:08:51 -0500

    Update userguide/conf.py
    Co-Authored-By: Warren Block <wblock@wonkity.com>

--- userguide/conf.py
+++ userguide/conf.py
@@ -95,7 +95,7 @@ rst_prolog = u'''
 .. |help-pin|             replace::  (Pin)
 .. |help-text|            replace::  (Help Text)
 .. |help-close|           replace::  (Close)
-.. |humanized-field|       replace:: This is a :ref:`human readable field <Humanized Fields>`.
+.. |humanized-field|       replace:: This is a :ref:`humanized field <Humanized Fields>`.
 .. |os-device|            replace:: operating system device
 .. |os-devices|           replace:: operating system devices
 .. |OS-Device|            replace:: Operating System Device
